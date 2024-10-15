5f117fc5c7df420090329911d24a5567871b189e
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-10-15 15:52:37 -0400

    aidl: light: Add timed blinking LED support based on HIDL implementation
    This adds the full timed blinking support from HIDL HAL. This
    implementation has also referenced the following AIDL HAL:
    
    https://github.com/LineageOS/android_hardware_xiaomi/commit/7329fd774f362d456ed0fbc0ff9215304669fb04#diff-0deab1c92301c972af651a47252c433024ecfcd07502c0165822a6a469a6d4ddR89
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- aidl/light/LED.cpp
+++ aidl/light/LED.cpp
@@ -29,6 +29,50 @@ bool LED::setBreath(uint32_t value) {
     return writeToFile(mBasePath + (mBreath ? "breath" : "blink"), value);
 }
 
+bool LED::setTimed(uint32_t value, uint32_t onMs, uint32_t offMs, uint32_t idx) {
+    // Abort if we have mBreath in use instead.
+    if (mBreath)
+        return false;
+
+    // LUT has 63 entries, we could theoretically use them as 3 (colors) * 21 (steps).
+    // However, the last LUT entries don't seem to behave correctly for unknown
+    // reasons, so we use 17 steps for a total of 51 LUT entries only.
+    static constexpr int kRampSteps = 16;
+    static constexpr int kRampMaxStepDurationMs = 15;
+    int pauseLo, pauseHi, stepDuration, start_idx = 0;
+
+    auto getScaledDutyPercent = [](int brightness) -> std::string {
+        std::string output;
+        for (int i = 0; i <= kRampSteps; i++) {
+            if (i != 0) {
+                output += ",";
+            }
+            output += std::to_string(i * 512 * brightness / (255 * kRampSteps));
+        }
+        return output;
+    };
+
+    if (kRampMaxStepDurationMs * kRampSteps > onMs) {
+        stepDuration = onMs / kRampSteps;
+        pauseHi = 0;
+        pauseLo = offMs;
+    } else {
+        stepDuration = kRampMaxStepDurationMs;
+        pauseHi = onMs - kRampSteps * stepDuration;
+        pauseLo = offMs - kRampSteps * stepDuration;
+    }
+
+    if (idx)
+        start_idx = (kRampSteps + 1) * idx;
+
+    return writeToFile(mBasePath + "start_idx", start_idx) &&
+            writeToFile(mBasePath + "duty_pcts", getScaledDutyPercent(value)) &&
+            writeToFile(mBasePath + "pause_lo", pauseLo) &&
+            writeToFile(mBasePath + "pause_hi", pauseHi) &&
+            writeToFile(mBasePath + "ramp_step_ms", stepDuration) &&
+            writeToFile(mBasePath + "blink", 1);
+}
+
 bool LED::setBrightness(uint32_t value) {
     return writeToFile(mBasePath + "brightness", value * mMaxBrightness / 0xFF);
 }
